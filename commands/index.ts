61656ed44acd0b17bb222c70c27e64125cd13202
Eugen Istoc <eugenistoc@gmail.com>
2021-06-20 01:18:30 -0400

    Remove non-existant command

--- commands/index.ts
+++ commands/index.ts
@@ -5,5 +5,4 @@ export default [
   'adonis-mikro-orm/build/commands/MigrationPending',
   'adonis-mikro-orm/build/commands/MigrationUp',
   'adonis-mikro-orm/build/commands/MigrationCreate',
-  'adonis-mikro-orm/build/commands/MigrationDrop',
 ]
